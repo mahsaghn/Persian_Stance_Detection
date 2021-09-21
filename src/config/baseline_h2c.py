8edd1a76f5da7567f61170605b0d4fe93d9ee3b7
mahsaghn <mahsa.ghaderan@gmail.com>
2021-09-21 19:17:07 +0000

    Output logistic regression tuning

--- src/config/baseline_h2c.py
+++ src/config/baseline_h2c.py
@@ -4,13 +4,13 @@ from typing import List
 @dataclass
 class FeatureExtractorConf:
   # important_words: List = field(default_factory=['؟', 'تکذیب',  'تکذیب شد', ':'])
-  dataset_path: str = '/home/mahsa/Desktop/final_project/stance_detection/dataset/h2c_new_dataset.csv'
-  stopWord_path: str = '/home/mahsa/Desktop/final_project/stance_detection/dataset/stop_words.txt'
-  ponctuations_path: str = '/home/mahsa/Desktop/final_project/stance_detection/dataset/ponctuations.txt'
-  uniq_claims_path: str = '/home/mahsa/Desktop/final_project/stance_detection/dataset/uniq_claims.txt'
+  dataset_path: str = '/home/ubuntu/ghaderan/stc/stance_detection/dataset/h2c_new_dataset.csv'
+  stopWord_path: str = '/home/ubuntu/ghaderan/stc/stance_detection/dataset/stop-words/nonverbal.txt'
+  ponctuations_path: str = '/home/ubuntu/ghaderan/stc/stance_detection/dataset/ponctuations.txt'
+  uniq_claims_path: str = '/home/ubuntu/ghaderan/stc/stance_detection/dataset/uniq_claims.txt'
   bert_model_path:str = 'HooshvareLab/bert-fa-zwnj-base'
-  stanford_models_path: str = '/home/mahsa/Desktop/final_project/stance_detection/dataset'
-  polarity_dataset_path: str = '/home/mahsa/Desktop/final_project/stance_detection/dataset/PerSent.xlsx'
+  stanford_models_path: str = '/home/ubuntu/ghaderan/stc/stance_detection/dataset'
+  polarity_dataset_path: str = '/home/ubuntu/ghaderan/stc/stance_detection/dataset/PerSent.xlsx'
   clean_claims_headlines: str = ''
   clean_claims: str = ''
   clean_headlines: str = ''
@@ -19,7 +19,7 @@ class FeatureExtractorConf:
   question_name: str = 'question'
   label_name: str = 'label'
   part_name: str = 'parts'
-  w2v_model_path: str = '/home/mahsa/Desktop/final_project/stance_detection/stance_detection/dataset/cc.fa.300.vec'
+  w2v_model_path: str = '/home/ubuntu/ghaderan/stc/stance_detection/dataset/cc.fa.300.vec'
   save_load_path: str = ''
   save_feature: bool = False
   load_path: bool = save_load_path,
@@ -27,19 +27,27 @@ class FeatureExtractorConf:
   load_if_exist: bool = False
   bow: bool = False
   w2v: bool = False
-  polarity: bool = False
+  polarity: bool = True
   tfidf: bool = True
-  similarity: bool = False
-  important_words: bool = False
-  is_question: bool = False
-  more_than2_parts: bool = False
-  tokenize_method: str = 'nltk'#nltk, stanford, bert, hazm
+  similarity: bool = True
+  important_words: bool = True
+  is_question: bool = True
+  more_than2_parts: bool = True
+  tokenize_method: str = 'hazm'#nltk, stanford, bert, hazm
+
+@dataclass
+class SVM:
+    C: float =  3.0
+    coef0 : float = 0.0
+    tol : float = 1e-3
+    kernel : str = 'rbf' #‘poly’, ‘rbf’, ‘sigmoid’
+    degree: int = 3 # degree of poly
 
 @dataclass
 class H2CBaselineConfig:
   save_path: str = '/models/'
   save_datasets:bool = False
-  load_path: str = '/home/mahsa/Desktop/final_project/stance_detection/stance_detection/dataset/seperated'
+  load_path: str = '/home/ubuntu/ghaderan/stc/stance_detection/dataset/seperated'
   load_if_exist: bool = False
   test_size: float = 0.2
   over_sample: bool = False
@@ -47,3 +55,4 @@ class H2CBaselineConfig:
   N_neighbors: int = 9
   Random_state: int = 88
   features: FeatureExtractorConf = FeatureExtractorConf()
+  svc : SVM = SVM()
